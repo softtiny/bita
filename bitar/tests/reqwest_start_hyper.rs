51669e35352897dce380639c5f91ea9270288883
Kunkka <lxj@321tips.com>
2025-02-27 14:47:26 +0800

    :poop:

--- bitar/tests/reqwest_start_hyper.rs
+++ bitar/tests/reqwest_start_hyper.rs
@@ -7,8 +7,8 @@ use hyper::body::Bytes;
 use hyper::{Request, Response};
 use hyper::rt::Executor;
 use hyper_util::rt::TokioIo;
+use tokio::join;
 use tokio::net::TcpListener;
-
 
 async fn hello(request: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
     println!("heloo...................");
@@ -28,7 +28,7 @@ where
     }
 }
 
-#[tokio::test]
+
 async fn hyper_server_start() -> Result<(), Box<dyn std::error::Error>> {
     let addr = SocketAddr::from(([127,0,0,1], 3000));
 
@@ -50,5 +50,22 @@ async fn hyper_server_start() -> Result<(), Box<dyn std::error::Error>> {
     }
 
 
+    Ok(())
+}
+
+async fn reqwest_request() -> Result<(), Box<dyn std::error::Error>> {
+    tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+    let url = "http://127.0.0.1:3000/";
+    let client = reqwest::Client::builder().http2_prior_knowledge().build().expect("failed to build client");
+
+    let res = client.get(url).send().await.expect("error request");
+    let data = res.text().await.expect("fail to text");
+    println!("response from server:{}",data);
+    Ok(())
+}
+
+#[tokio::test]
+async fn request_with_join() -> Result<(), Box<dyn std::error::Error>> {
+    join!(hyper_server_start(),reqwest_request());
     Ok(())
 }
