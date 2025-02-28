0bd3271122247e29d6fbe4613181bb0e95a6ff51
Kunkka <lxj@321tips.com>
2025-02-28 16:08:58 +0800

    :construction: Work in progress.

--- bitar/tests/reqwest_start_hyper.rs
+++ bitar/tests/reqwest_start_hyper.rs
@@ -76,23 +76,51 @@ async fn request_with_join() -> Result<(), Box<dyn std::error::Error>> {
 
 
 // Example a  start ---- aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-async fn handle_request_wait_5(_req: Request<hyper::body::Incoming>) -> Result<Response<hyper::body::Bytes>, Infallible> {
+async fn handle_request_wait_5(_req: Request<hyper::body::Incoming>) -> Result<Response<Full<hyper::body::Bytes>>, Infallible> {
     tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-    Ok(Response::new(Bytes::from("hello world")))
+    Ok(Response::new(Full::from( Bytes::from("hello world"))))
+}
+
+async fn reqwest_request_2s() -> Result<String, Box<dyn std::error::Error>>{
+    let url = "http://127.0.0.1:3000";
+    let client = reqwest::Client::builder().http2_prior_knowledge().timeout(std::time::Duration::from_secs(2)).build().expect("failed to build client");
+
+    let res = client.get(url).send().await?;
+    let msg = res.text().await.expect("get text err");
+    println!("msg is: {}",msg);
+    Ok(msg)
 }
 
 #[tokio::test]
 async fn request_timeout_work() -> Result<(), Box<dyn std::error::Error>> {
     // Server setup
     let addr = SocketAddr::from(([127,0,0,1],3000));
-    let listener = TcpListener::bind(addr).await.expect("bind prot erro");
-    tokio::task::spawn(async move {
-        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-        println!("sleep 5s");
+    let listener = TcpListener::bind(addr).await.expect("bind port erro");
+    let server_task = tokio::task::spawn(async move {
+        loop{
+            let (stream,_err) = listener.accept().await.expect("accpet listen err");
+            let io = TokioIo::new(stream);
+            tokio::task::spawn(async move {
+                //TokioExecutor
+                if let Err(err) = http2::Builder::new(TokioExecutor)
+                    .serve_connection(io,service_fn(handle_request_wait_5))
+                    .await {
+                    eprintln!("http2 server error ..{:?}",err);
+                }
+            });
+        }
     });
     println!("new on mian");
-    tokio::time::sleep(std::time::Duration::from_secs(8)).await;
-    println!("after 8s");
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    tokio::task::spawn(async {
+        tokio::time::sleep(std::time::Duration::from_secs(1));
+        println!("timeout wait 1s,still wating");
+    });
+    if let Err(err)  = reqwest_request_2s().await {
+        println!("timout 2s err occur:{:?}",err);
+    }
+    println!("after client req s");
+    server_task.abort();
     Ok(())
 }
 
